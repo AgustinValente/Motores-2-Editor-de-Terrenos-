d7ac84ed2b72b665c1fabca66df8fc4512d46123
AgustinValente <Alcaf123>
2019-10-31 19:46:41 -0300

    Update1
    Solo un Script

---
+++ Motores-2-Editor-de-Terrenos/Assets/Editor/ProofEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Proof))]
+[ExecuteInEditMode]
+public class ProofEditor : Editor
+{
+    Proof _target;
+   
+    private static Vector3 position;
+    
+
+    private void OnEnable()
+    {
+        _target = (Proof)target;
+    }
+
+    private void OnSceneGUI()
+    {
+        /*  if (Physics.Raycast(_target.ray, out _target.hit))
+          {
+              Handles.color = Color.red;
+              Handles.DrawSolidDisc(_target.hit.transform.position, _target.hit.transform.forward, 15);
+          }
+          //  Handles.color = Color.blue;
+          // Handles.DrawSolidDisc(Camera.current.WorldToScreenPoint(Input.mousePosition),Vector3.forward , 15);*/
+        /*  Vector3 mousePosition = Event.current.mousePosition;
+          Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
+          mousePosition = ray.origin;
+          Handles.DrawLine(_target.transform.position + Vector3.up, mousePosition);
+          Handles.color = Color.red;
+          Handles.DrawSolidDisc(mousePosition,_target.transform.forward, 5);*/
+
+        Ray mousePosition;
+        mousePosition = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        SceneView.RepaintAll();
+        RaycastHit hit;
+        if (Physics.Raycast(mousePosition, out hit))
+        {
+            Handles.color = Color.red;
+            Handles.DrawSolidDisc(hit.point, -hit.transform.up, 10);
+            HandleUtility.Repaint();
+        }
+        HandleUtility.Repaint();
+    }
+    
+}
---
+++ Motores-2-Editor-de-Terrenos/Assets/Proof.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Proof : MonoBehaviour
+{
+    public Camera _camera;
+    public RaycastHit hit;
+    public Ray ray;
+    public float bord1X;
+    public float bord2X;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    //    ray = _camera.ScreenPointToRay(Input.mo);
+    }
+}
