b4b3b3d21994c52feebca7f5d430a145b779b1ee
AgustinValente <Alcaf123>
2019-11-01 09:50:23 -0300

    Update2
    Una actualizacion de los handles

--- Motores-2-Editor-de-Terrenos/Assets/Editor/ProofEditor.cs
+++ Motores-2-Editor-de-Terrenos/Assets/Editor/ProofEditor.cs
@@ -7,43 +7,73 @@ using UnityEditor;
 [ExecuteInEditMode]
 public class ProofEditor : Editor
 {
-    Proof _target;
    
-    private static Vector3 position;
-    
-
+    Proof _target;   
+    Ray mousePosition;
+    RaycastHit hit;
+    private string[] toolbarOptions = new string[] {"Ninguno","Ciculo", "Cuadrado"};
+    int _currentToolbarSelection;
     private void OnEnable()
     {
         _target = (Proof)target;
     }
 
+ 
+
     private void OnSceneGUI()
     {
-        /*  if (Physics.Raycast(_target.ray, out _target.hit))
-          {
-              Handles.color = Color.red;
-              Handles.DrawSolidDisc(_target.hit.transform.position, _target.hit.transform.forward, 15);
-          }
-          //  Handles.color = Color.blue;
-          // Handles.DrawSolidDisc(Camera.current.WorldToScreenPoint(Input.mousePosition),Vector3.forward , 15);*/
-        /*  Vector3 mousePosition = Event.current.mousePosition;
-          Ray ray = HandleUtility.GUIPointToWorldRay(mousePosition);
-          mousePosition = ray.origin;
-          Handles.DrawLine(_target.transform.position + Vector3.up, mousePosition);
-          Handles.color = Color.red;
-          Handles.DrawSolidDisc(mousePosition,_target.transform.forward, 5);*/
+        Handles.BeginGUI();
+        var v = EditorWindow.GetWindow<SceneView>().camera.pixelRect;
+        GUILayout.BeginArea(new Rect(20, 20, 300, 175));
+        _target.radius = EditorGUILayout.FloatField("Radio", _target.radius);
 
-        Ray mousePosition;
+        _target.opacity = EditorGUILayout.Slider("opacidad", _target.opacity,0.1f,1);
+
+        _currentToolbarSelection = GUILayout.Toolbar(_currentToolbarSelection, toolbarOptions);
+        GUILayout.EndArea();
+        Handles.EndGUI();
+        if (_currentToolbarSelection == 1)
+        {
+            SphereHandles();
+        }
+        if (_currentToolbarSelection == 2)
+        {
+            QuadHandles();
+        }
+
+    }
+
+    void SphereHandles()
+    {
         mousePosition = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         SceneView.RepaintAll();
-        RaycastHit hit;
         if (Physics.Raycast(mousePosition, out hit))
         {
-            Handles.color = Color.red;
-            Handles.DrawSolidDisc(hit.point, -hit.transform.up, 10);
+            Handles.color = new Color(1,0,0, _target.opacity);
+            Handles.DrawSolidDisc(hit.point, -hit.transform.up, _target.radius);
             HandleUtility.Repaint();
         }
         HandleUtility.Repaint();
     }
-    
+    void QuadHandles()
+    {
+        var verts = new Vector3[]
+        {
+            new Vector3(hit.point.x -_target.radius,hit.point.y,hit.point.z -_target.radius),
+            new Vector3(hit.point.x -_target.radius,hit.point.y,hit.point.z +_target.radius),
+            new Vector3(hit.point.x +_target.radius,hit.point.y,hit.point.z +_target.radius),
+            new Vector3(hit.point.x +_target.radius,hit.point.y,hit.point.z -_target.radius)
+        };
+        mousePosition = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        SceneView.RepaintAll();
+        if (Physics.Raycast(mousePosition, out hit))
+        {
+            Handles.color = Color.red;
+            // Handles.DrawSolidDisc(hit.point, -hit.transform.up, _target.radius);
+           Handles.DrawSolidRectangleWithOutline(verts, new Color(1, 0, 0, _target.opacity),new Color(1, 0, 0, _target.opacity));
+            HandleUtility.Repaint();
+        }
+        HandleUtility.Repaint();
+    }
+
 }
--- Motores-2-Editor-de-Terrenos/Assets/Script/Proof.cs
+++ Motores-2-Editor-de-Terrenos/Assets/Script/Proof.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Proof : MonoBehaviour
 {
-    public Camera _camera;
+ 
     public RaycastHit hit;
     public Ray ray;
-    public float bord1X;
-    public float bord2X;
-
+    public float radius;
+    public float opacity;
     // Start is called before the first frame update
     void Start()
     {
