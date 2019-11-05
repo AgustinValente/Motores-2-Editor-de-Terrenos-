74028f358ddcca341f6ec9f8fdb2de746c71f423
matinaipapa <44042851+matinaipapa@users.noreply.github.com>
2019-11-05 11:18:06 -0300

    Previsualizar Objects/Place object on selected terrain. (center)

--- Motores-2-Editor-de-Terrenos/Assets/Editor/PrefabsEditor.cs
+++ Motores-2-Editor-de-Terrenos/Assets/Editor/PrefabsEditor.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 
 public class PrefabsEditor : EditorWindow
 {
-    public Object target;
+    public GameObject target;
     public GameObject prefab;
     public bool posXBool;
     public bool posXboolNegative;
@@ -14,6 +14,9 @@ public class PrefabsEditor : EditorWindow
     public bool posYboolNegative;
     public bool posZBool;
     public bool posZboolNegative;
+    Editor the;
+    public bool lefTerrainPos;
+   
 
 
     [MenuItem("Prefab Maker/PrefabEditor")]
@@ -37,9 +40,11 @@ public class PrefabsEditor : EditorWindow
            }*/
 
 
-
+        ShowPreview();
         if (Selection.activeGameObject != null)
         {
+            
+
             float posX = Selection.activeGameObject.GetComponent<Transform>().transform.position.x;
             float posY = Selection.activeGameObject.GetComponent<Transform>().transform.position.y;
             float posZ = Selection.activeGameObject.GetComponent<Transform>().transform.position.z;
@@ -113,11 +118,71 @@ public class PrefabsEditor : EditorWindow
 
             }
 
+        }
 
+        SpawnPosition();
 
+    }
+
+    private void ShowPreview()
+    {
+        
+        var _selection = Selection.activeGameObject;
+        EditorGUILayout.LabelField("Selection preview");
+        _selection = (GameObject)EditorGUILayout.ObjectField(_selection, typeof(GameObject), true);
+
+        GUIStyle myGuiStyle = new GUIStyle();
+        myGuiStyle.normal.background = EditorGUIUtility.whiteTexture;
+        if (_selection != null)
+        {
+            EditorGUI.DrawPreviewTexture(GUILayoutUtility.GetRect(50, 200),AssetPreview.GetAssetPreview(_selection));
+        }
+        if (_selection == null)
+        {
+            EditorGUI.DrawPreviewTexture(GUILayoutUtility.GetRect(50, 200), AssetPreview.GetAssetPreview(myGuiStyle.normal.background = EditorGUIUtility.whiteTexture));
 
         }
 
+
+    }
+
+    public void SpawnPosition()
+    {
+        EditorGUILayout.LabelField("Select on Which Terrain you Want to Create an Object: ");
+
+        target = (GameObject)EditorGUILayout.ObjectField(target, typeof(GameObject), true);
+
+        float posX = target.GetComponent<Transform>().transform.position.x;
+        float posY = target.GetComponent<Transform>().transform.position.y;
+        float posZ = target.GetComponent<Transform>().transform.position.z;
+
+        Quaternion rotation = new Quaternion(0f, 0f, 0f, 0f);
+        if (target != null)
+        { 
+                if (GUILayout.Button("Reset Position"))
+                {
+                //prueba 
+                var resetPosX = target.transform.position.x;
+                var resetPosY = target.transform.position.y + Selection.activeGameObject.transform.localScale.y;
+                var resetPosZ = target.transform.position.z;
+                //center object on Terrain
+                Selection.activeGameObject.transform.localPosition = new Vector3(resetPosX, resetPosY, resetPosZ);
+
+
+            /*if (EditorGUILayout.Toggle("Make left of Terrain", lefTerrainPos))
+            {
+
+        if (GUILayout.Button("Spawn Prefab on Side/Center"))
+            {
+            Vector3 position = new Vector3(posX, posY, posZ);
+            posX = target.transform.position.x - target.transform.localScale.x;
+            Selection.activeObject = Instantiate(Selection.activeGameObject, position, rotation);
+            }            
+        }*/
+            }
+
+        
+        }
     }
 
 }
